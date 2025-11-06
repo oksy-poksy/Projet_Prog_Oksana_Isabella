6c2f89a8b54bec857dfb5043082990008ddd8894
oksy-poksy <sana.ptashnyk@gmail.com>
2025-11-06 10:04:52 +0100

    commit AJOUT FICHIER JEUfactice

---
+++ JeuFactice.py
@@ -0,0 +1,30 @@
+# fichier pour les classes
+#on utilise numpy pour la structure de code
+from builtins import Exception
+
+
+class JeuFactice: #architecture du code
+    def __init__(self): pass
+    def premier_tour(self): pass
+    def tour_de_jeu(self, joueur, mat): pass #choisir la case dans laquelle le joueur veut jouer, dans quelle matrixce
+    #cette fct doit retiurner la matrice dans laquelle le prochain tour se deroule
+    def signe_de_joueur1(self): pass
+    def signe_de_joueur2(self): pass
+    def reussite_matrice(self, joueur, mat): pass
+    #complete la matrice 10
+    def fin_jeu(self, joeur): pass
+
+
+    # on va faire 10 matrices
+
+
+class Exception():
+    def __init__(self): pass
+
+    class ErreurClick(Exception): pass
+
+class Matrices:
+    def __init__(self): pass
+
+class Pokemons:
+    def __init__(self): pass
